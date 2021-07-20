8a82f988b0bd13ba2ef67e0547d16f517ff1d9da
zhangjie <njtechzhangjie@gmail.com>
2021-07-21 06:52:52 +0800

    'init'

---
+++ DataStructure/cmake-build-debug/CMakeFiles/DataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure.dir/main.c.o"
+  "DataStructure"
+  "DataStructure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
